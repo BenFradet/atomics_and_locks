a822fae1caf42ea9585bad36e0ceffebeadde39b
benfradet <benjamin.fradet@gmail.com>
2025-04-04 21:59:45 +0200

    fence

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use atomics::statistics::statistics;
 
 pub mod atomics;
+pub mod memory_ordering;
 
 fn main() {
     statistics();
---
+++ src/memory_ordering/fence.rs
@@ -0,0 +1,32 @@
+use std::{sync::atomic::{fence, AtomicBool, Ordering}, thread, time::Duration};
+
+// non-atomic shared variable
+static mut DATA: [u64; 10] = [0; 10];
+#[allow(clippy::declare_interior_mutable_const)]
+const ATOMIC_FALSE: AtomicBool = AtomicBool::new(false);
+static READY: [AtomicBool; 10] = [ATOMIC_FALSE; 10];
+
+pub fn fences() {
+    for i in 0..10 {
+        thread::spawn(move || {
+            let data = some_calculation(i);
+            unsafe { DATA[i] = data };
+            READY[i].store(true, Ordering::Release);
+        });
+    }
+    thread::sleep(Duration::from_millis(500));
+    let ready: [bool; 10] = std::array::from_fn(|i| READY[i].load(Ordering::Relaxed));
+    if ready.contains(&true) {
+        // only acquire if there is data to be read
+        fence(Ordering::Acquire);
+        for i in 0..10 {
+            if ready[i] {
+                println!("data {i} = {}", unsafe { DATA[i] });
+            }
+        }
+    }
+}
+
+fn some_calculation(i: usize) -> u64 {
+    i as u64
+}
